952fbb79bc91c4c4aa76e92ebe100977e23024fc
15135282519 <15135282519@163.com>
2023-04-17 13:12:19 +0800

    Initial commit

---
+++ src/main/java/net/yxy/yxy_mod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.yxy.yxy_mod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.yxy.yxy_mod.yxyMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		yxyMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
